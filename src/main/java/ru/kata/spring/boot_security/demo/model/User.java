6db933a8623e751584313010f24ff969856dc515
Updmt <Wanttodo@mail.ru>
2022-09-09 11:40:52 +0300

    rrr

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -38,11 +38,11 @@ public class User implements UserDetails{
     @Column(name="password")
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles;
 
     public User() {}
 
